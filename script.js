0ada2684ded9e5f181f4f9fbc5365a943f797a7d
Afnan Ahmad <afnanahmad9565@gmail.com>
2023-10-23 13:25:39 +0530

    JS Added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Defining Variables
+let action = document.getElementById("action");
+let reaction = document.getElementById("reaction");
+let $1 = document.getElementById("1");
+let $2 = document.getElementById("2");
+let $3 = document.getElementById("3");
+let $4 = document.getElementById("4");
+let $5 = document.getElementById("5");
